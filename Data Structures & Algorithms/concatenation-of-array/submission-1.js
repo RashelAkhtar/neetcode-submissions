ea872e965cfb785252000f6736e49e5cfd95530e
RashelAkhtar <akhtarrashel408@gmail.com>
2026-06-15 20:53:25 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.js
@@ -0,0 +1,13 @@
+class Solution {
+    getConcatenation(nums) {
+        let n = nums.length;
+        let ans = new Array(n*2);
+
+        for (let i=0; i<n; i++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+
+        return ans;
+    }
+}
