94e201ffc09af1de138a43466d4431ee85b1289e
RashelAkhtar <akhtarrashel408@gmail.com>
2026-07-14 11:15:57 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.js
@@ -0,0 +1,15 @@
+class Solution {
+    hasDuplicate(nums) {
+        let map = new Map();
+
+        for (let i=0; i<nums.length; i++) {
+
+            if (map.has(nums[i])) {
+                return true;
+            }
+            map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+        }
+
+        return false;
+    }
+}
