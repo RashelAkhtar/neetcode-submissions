f011481dddc0ee43c611afd52462f3c80a4ea6aa
RashelAkhtar <akhtarrashel408@gmail.com>
2026-07-14 11:23:48 +0530

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    containsNearbyDuplicate(nums, k) {
+        let map = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            if (map.has(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k) {
+                return true;
+            }
+            map.set(nums[i], i);
+        }
+
+        return false;
+    }
+}
