4423785617889ece6198325d24bbf3b60901d0f6
RashelAkhtar <akhtarrashel408@gmail.com>
2026-07-19 11:48:28 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    searchMatrix(matrix, target) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] === target) {
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+}
